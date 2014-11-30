65f53690267d654e852945a24fa513bb949906ae
Michel Käser <michel.kaeser.91@gmail.com>
2014-12-01 00:30:05 +0100

    root check; sudo not needed anymore; check if yum exists..may be Arch linux or another one; create the public dir

--- scripts/setup_docker_host.sh
+++ scripts/setup_docker_host.sh
@@ -13,22 +13,30 @@
 # last updated: 30.11.2014
 #
 
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be executed as root user."
+  exit 1
+fi
+
 echo "Note: During installation, you'll be prompted with a dialog to configure the LDAP client."
 echo "Make sure you enter the correct information for your system."
 
 # detect the package manager
-if `which apt-get &> /dev/null`; then
+if [ `which apt-get &> /dev/null` ]; then
     PS="deb"
     INSTALL="apt-get install -y"
-else
+elif [ `which yum &> /dev/null` ]; then
     PS="rpm"
     INSTALL="yum install -y"
+else
+    echo "Your distribution is not supported yet. Aborting the setup."
+    exit 1
 fi
 
 # install prequisites
 $INSTALL curl sed sudo tar
 # install docker.io
-if $PS == "deb"; then
+if [ $PS == "deb" ]; then
     $INSTALL docker.io
 else
     $INSTALL docker
@@ -36,14 +44,20 @@ fi
 # install docker-bash/-ssh (optional)
 curl --fail -L -O https://github.com/phusion/baseimage-docker/archive/master.tar.gz
 tar xzf master.tar.gz
-sudo ./baseimage-docker-master/install-tools.sh
+./baseimage-docker-master/install-tools.sh
 rm -rf master.tar.gz baseimage-docker-mages
 # pull the base image for our templates
 docker pull phusion/baseimage
 
 # create the directories for the users and shares
-mkdir -p /srv/homes
-mkdir -p /srv/shares
+DATA="/srv/ipynbsrv"
+mkdir -p $DATA
+mkdir -p $DATA/homes
+mkdir -p $DATA/public
+mkdir -p $DATA/shares
+# ensure secure permissions. just in cast a custom umask is set
+chown -R root:root $DATA
+chmod -R 755 $DATA
 
 # install the LDAP client tools
 # we need to know all LDAP users because the home/share directories will belong to them
