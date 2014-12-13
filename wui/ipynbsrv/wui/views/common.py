6bc428dbc82d82b16fd7c592566655be73531412
Michel Käser <michel.kaeser.91@gmail.com>
2014-12-13 15:58:18 +0100

    workspace init

--- wui/ipynbsrv/wui/views/common.py
+++ wui/ipynbsrv/wui/views/common.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from djproxy.views import HttpProxy
 from ipynbsrv.wui.auth.checks import login_allowed
 
 
@@ -11,3 +13,17 @@ def dashboard(request):
     }
 
     return render(request, 'wui/dashboard.html', context)
+
+
+@user_passes_test(login_allowed)
+class WorkspaceProxy(HttpProxy):
+    """
+    """
+    base_url = settings.WORKSPACE_PROXY_URL
+
+
+    """
+    """
+    def dispatch(self, request, *args, **kwargs):
+        # TODO: check if user is allowed here
+        return super(WorkspaceProxy, self).dispatch(request, *args, **kwargs)
