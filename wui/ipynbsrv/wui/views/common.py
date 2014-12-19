b281748786c670774867141ed386c1f2b9ded579
Michel Käser <michel.kaeser.91@gmail.com>
2014-12-19 21:36:04 +0100

    init...works without the auth-request module

--- wui/ipynbsrv/wui/views/common.py
+++ wui/ipynbsrv/wui/views/common.py
@@ -1,10 +1,7 @@
-from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import redirect, render
-from djproxy.views import HttpProxy
+from django.http.response import HttpResponse
+from django.shortcuts import render
 from ipynbsrv.wui.auth.checks import login_allowed
-from ipynbsrv.wui.models import Container
 
 
 ""
@@ -17,26 +14,12 @@ def dashboard(request):
     return render(request, 'wui/dashboard.html', context)
 
 
-class WorkspaceProxy(HttpProxy):
-    """
-    """
-    base_url = settings.WORKSPACE_PROXY_URL
+"""
+"""
+def workspace_auth_check(request):
+    # TODO
+    print request.user
+    print request.path
+    print request
 
-
-    """
-    """
-    def dispatch(self, request, *args, **kwargs):
-        splits = request.path.split('/')
-        if len(splits) >= 3:
-            container = Container.objects.filter(ports=splits[2]).first()
-            if container:
-                if container.owner == request.user:
-                    return super(WorkspaceProxy, self).dispatch(request, *args, **kwargs)
-                else:
-                    messages.error(request, "You have no permissions to access this container.")
-            else:
-                messages.error(request, "Container does not exist.")
-        else:
-            messages.error(request, "Invalid URL.")
-
-        return redirect('containers')
+    return HttpResponse(status=200)
