e9d8661edb8664be4853d922862ae1a024148bb4
Michel Käser <michel.kaeser.91@gmail.com>
2014-12-20 22:30:57 +0100

    fix invalid keyword error

--- wui/ipynbsrv/wui/views/system.py
+++ wui/ipynbsrv/wui/views/system.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 Error 404 - Page not found error view.
 """
 def error_404(request):
-    return render(request, 'wui/errors/404.html', context = {
+    return render(request, 'wui/errors/404.html', {
         'title': "Error 404"
     })
 
@@ -14,6 +14,6 @@ def error_404(request):
 Error 500 - Internal Server Error view.
 """
 def error_500(request):
-    return render(request, 'wui/errors/500.html', context = {
+    return render(request, 'wui/errors/500.html', {
         'title': "Error 500"
     })
