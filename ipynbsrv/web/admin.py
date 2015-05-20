f014680315d4744ff347320e5f7312e2c35482bd
Christian Glatthard <christian.glatthard@students.fhnw.ch>
2015-05-20 14:46:18 +0200

    explicit imports, InlineAdmin for User

--- ipynbsrv/web/admin.py
+++ ipynbsrv/web/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from ipynbsrv.core.models import *
+from ipynbsrv.core.models import Backend, Container, Image, Share, Tag, IpynbUser, IpynbGroup, Server
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
 
 
 class BackendAdmin(admin.ModelAdmin):
@@ -55,9 +57,34 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ['label']
 
 
+class IpynbUserInline(admin.StackedInline):
+    model = IpynbUser
+    can_delete = False
+    verbose_name_plural = 'Ipynb Frontend User'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (IpynbUserInline, )
+
+
+class IpynbGroupInline(admin.StackedInline):
+    model = IpynbGroup
+    can_delete = False
+    verbose_name_plural = 'Ipynb Frontend User'
+
+
+class GroupAdmin(GroupAdmin):
+    inlines = (IpynbGroupInline, )
+
+
 admin.site.register(Backend, BackendAdmin)
 admin.site.register(Container, ContainerAdmin)
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Server, ServerAdmin)
 admin.site.register(Share, ShareAdmin)
 admin.site.register(Tag, TagAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
