94eeebac1d793325c0092a654fbb3dd4e7225ac2
Michel Käser <michel.kaeser.91@gmail.com>
2015-07-18 11:22:25 +0200

    fix login_allowed decorater

--- ipynbsrv/core/auth/checks.py
+++ ipynbsrv/core/auth/checks.py
@@ -1,4 +1,4 @@
-from ipynbsrv.conf import global_vars
+from ipynbsrv.core.models import BackendUser
 
 
 def login_allowed(user):
@@ -8,8 +8,4 @@ def login_allowed(user):
     We do not want to allow non-UserBackend users to access the application
     (because we need the LDAP entry for the shares etc.) so we check that here.
     """
-    if not user.username:
-        return False
-    else:
-        user_backend = global_vars.USER_BACKEND
-        return user_backend.user_exists(user.username)
+    return BackendUser.objects.filter(user=user).exists()
