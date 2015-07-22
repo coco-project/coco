45f0cbed071b57d19aead4abfe8b6b2daad257c3
Christian Glatthard <christian.glatthard@students.fhnw.ch>
2015-07-22 07:54:03 +0200

    add SHARE_GROUP_PREFIX setting

--- ipynbsrv/core/settings.py
+++ ipynbsrv/core/settings.py
@@ -13,13 +13,17 @@ STORAGE_DIR_HOME = 'homes/'
 STORAGE_DIR_PUBLIC = 'public/'
 STORAGE_DIR_SHARES = 'shares/'
 
+"""
+Setting storing the prefix for the helper groups used to manage access to shares.
+"""
+SHARE_GROUP_PREFIX = 'SHR_'
 
 """
 Setting storing the user ID offset to be added to internal ldap users
 """
 USER_ID_OFFSET = 2500
 
-'''
+"""
 Setting storing the group ID offset to be added to internal ldap groups
-'''
+"""
 GROUP_ID_OFFSET = 5500
