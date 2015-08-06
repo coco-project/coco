9f3d933365ca53339118df7953304e2dd11e5d8d
Michel Käser <mail@michelkaeser.ch>
2015-08-06 14:17:14 +0200

    strictly separate users from admins

--- ipynbsrv/core/signals/shares.py
+++ ipynbsrv/core/signals/shares.py
@@ -11,6 +11,14 @@ from os import path
 storage_backend = get_storage_backend()
 
 
+@receiver(collaboration_group_admin_added)
+def add_admin_to_share_groups(sender, group, user, **kwargs):
+    """
+    Add the admin to all share groups the entered group has access to.
+    """
+    add_user_to_share_groups(sender, group, user, kwargs)
+
+
 @receiver(share_created)
 def add_creator_to_share_group(sender, share, **kwargs):
     """
@@ -36,6 +44,10 @@ def add_group_members_to_share_group(sender, share, group, **kwargs):
     Add all members from the access group to the share group.
     """
     if share is not None and group is not None:
+        if group.creator:
+            share.add_member(group.creator)
+        for admin in group.admins.all():
+            share.add_member(admin)
         for user in group.get_members():
             share.add_member(user)
 
@@ -89,18 +101,30 @@ def remove_group_members_from_share_group(sender, share, group, **kwargs):
     Remove all members from the access group from the share group.
     """
     if share is not None and group is not None:
-        for user in group.get_members():
+        user_set = list(group.admins.all()) + list(group.get_members())
+        if group.creator:
+            user_set.append(group.creator)
+        for user in user_set:
             leave = False
             if user == share.owner:
                 leave = True
             else:
                 for access_group in share.access_groups.all():
                     if access_group != group:
-                        if access_group.user_is_member(user):
+                        if user == access_group.creator or access_group.user_is_admin(user) \
+                                or access_group.user_is_member(user):
                             leave = True
                             break
             if not leave:
                 share.remove_member(user)
+
+
+@receiver(collaboration_group_admin_removed)
+def remove_admin_from_share_groups(sender, group, user, **kwargs):
+    """
+    Remove the admin from share groups the group had access to.
+    """
+    remove_user_from_share_groups(sender, group, user, kwargs)
 
 
 @receiver(collaboration_group_member_removed)
@@ -120,7 +144,8 @@ def remove_user_from_share_groups(sender, group, user, **kwargs):
             else:
                 for access_group in share.access_groups.all():
                     if access_group != group:
-                        if access_group.user_is_member(user):
+                        if user == access_group.creator or access_group.user_is_admin(user) \
+                                or access_group.user_is_member(user):
                             leave = True
                             break
             if not leave:
