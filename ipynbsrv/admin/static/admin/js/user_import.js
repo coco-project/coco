237abf5778c37900a2a4112832a3e66eceefe195
Christian Glatthard <christian.glatthard@students.fhnw.ch>
2015-08-09 16:26:06 +0200

    fix .gitignore to only exclude global static dir

---
+++ ipynbsrv/admin/static/admin/js/user_import.js
@@ -0,0 +1,5 @@
+(function($) {
+    $(document).ready(function($) {
+        $("#content-main").append('<div class="module"><a href="/admin/auth/user/import_users/"><button>Import all users from User Backend</button></a></div>');
+    });
+})(django.jQuery);
