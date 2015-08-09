3f858bce1f1d79d34334e084e794781416096a52
Christian Glatthard <christian.glatthard@students.fhnw.ch>
2015-08-09 17:11:09 +0200

    move import users button to .object-tools list in upper right corner of admin, also hide it on user detail view

--- ipynbsrv/admin/static/admin/js/user_import.js
+++ ipynbsrv/admin/static/admin/js/user_import.js
@@ -1,5 +1,14 @@
 (function($) {
     $(document).ready(function($) {
-        $("#content-main").append('<div class="module"><a href="/admin/auth/user/import_users/"><button>Import all users from User Backend</button></a></div>');
+    	// check if history link exists in site, if yes, we are on the user detail site and don't want to include the button
+    	if ($(".historylink").length == 0) {
+    		// check if .object-tools exists
+	    	if ($(".object-tools").length == 0) {
+	    		$("#content-main").prepend('<ul class="object-tools"><li><a class="addlink" href="/admin/auth/user/import_users/">Import all users from User Backend</a></li></ul>');
+	    	} else {
+	        	$(".object-tools").prepend('<li><a class="addlink" href="/admin/auth/user/import_users/">Import all users from User Backend</a></li>');
+	        	}
+    	}
+    	
     });
 })(django.jQuery);
